1f9decfe84e6b595fb306aee6b967be6e57e2289
Aakritiparajuli2003 <121143822+Aakritiparajuli2003@users.noreply.github.com>
2024-12-02 20:42:39 +0545

    css

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-<<<<<<< HEAD
-    <meta name="viewport" content="widthdevice-width", initial-scale="1.0">
-=======
-    <meta name="viewport" content="width=device-width", initial-scale="1.0">
->>>>>>> 5fad978465bea0935b66883498a3b75cbaec72aa
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register and Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css">
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container" id="signup">
