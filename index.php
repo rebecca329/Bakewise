f140cf6a6bcac283b624035d09fa1463b426add7
Aakritiparajuli2003 <121143822+Aakritiparajuli2003@users.noreply.github.com>
2024-12-04 18:37:37 +0545

    login and register form

--- index.php
+++ index.php
@@ -8,60 +8,70 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container" id="signup" style="display:none;">
-        <h1 class="form-title">Register</h1>
-        <form method="post" action="">
-            <div class="input-group">
-                <input type="text" name="fName" id="fName" placeholder="First Name" required>
-                <i class="fas fa-user"></i>
-            </div>
-            <div class="input-group">
-                <input type="text" name="lName" id="lName" placeholder="Last Name" required>
-                <i class="fas fa-user"></i>
-            </div>
-            <div class="input-group">
-                <input type="email" name="email" id="email" placeholder="Email" required>
-                <i class="fas fa-envelope"></i>
-            </div>
-            <div class="input-group">
-                <input type="password" name="password" id="password" placeholder="Password" required>
-                <i class="fas fa-lock"></i>
-            </div>
-        </form>
-        <p class="or">--------------or--------------</p>
-        <div class="icons">
-            <i class="fab fa-google"></i>
-            <i class="fab fa-facebook"></i>
+<div class="container" id="signUp" style="display: none;">
+    <h1 class="form-title">Register</h1>
+    <form method="post" action="">
+        <div class="input-group">
+            <i class="fas fa-user"></i>
+            <input type="text" name="fName" id="fName" placeholder="First Name" required>
+            <label for="fName">First Name</label>
         </div>
-        <div class="button-group">
-            <button type="submit">Register</button>
+        <div class="input-group">
+            <i class="fas fa-user"></i>
+            <input type="text" name="lName" id="lName" placeholder="Last Name" required>
+            <label for="lName">Last Name</label>
         </div>
-        <div class="links">
-            <p>Already Have an Account?</p>
-            <a href="#">Login</a>
+        <div class="input-group">
+            <i class="fas fa-envelope"></i>
+            <input type="email" name="email" id="email" placeholder="Email" required>
+            <label for="email">Email</label>
         </div>
+        <div class="input-group">
+            <i class="fas fa-lock"></i>
+            <input type="password" name="password" id="password" placeholder="Password" required>
+            <label for="password">Password</label>
+        </div>
+        <input type="submit" class="btn" value="Sign Up" name="SignUp">
+    </form>
+    <p class="or">--- or ---</p>
+    <div class="icons">
+        <i class="fab fa-google"></i>
+        <i class="fab fa-facebook"></i>
     </div>
-    <div class="container" id="login">
-        <h1 class="form-title">Login</h1>
-        <form method="post" action="">
-            <div class="input-group">
-                <input type="email" name="email" id="email" placeholder="Email" required>
-                <i class="fas fa-envelope"></i>
-            </div>
-            <div class="input-group">
-                <input type="password" name="password" id="password" placeholder="Password" required>
-                <i class="fas fa-lock"></i>
-            </div>
-            <p class="recover">
-                <a href="#">Recover Password</a></p>
-        </form>
-        <p class="or">--------------or--------------</p>
-        <div class="links">
-            <p>Don't have account?</p>
-           <button id="LoginButton">Login</button>
-        </div>
+    <div class="links">
+        <p>Already have an account?</p>
+        <button id="SignIn">Sign In</button> 
     </div>
+</div>
+  
+<div class="container" id="signIn">
+    <h1 class="form-title">Sign In</h1>
+    <form method="post" action="">
+        <div class="input-group">
+            <i class="fas fa-envelope"></i>
+            <input type="email" name="email" id="email-login" placeholder="Email" required>
+            <label for="email-login">Email</label>
+        </div>
+        <div class="input-group">
+            <i class="fas fa-lock"></i>
+            <input type="password" name="password" id="password-login" placeholder="Password" required>
+            <label for="password-login">Password</label>
+        </div>
+        <p class="recover">
+            <a href="#" class="link">Recover Password</a>
+        </p>
+        <input type="submit" class="btn" value="Sign In" name="SignIn">
+    </form>
+    <p class="or">--- or ---</p>
+    <div class="icons">
+        <i class="fab fa-google"></i>
+        <i class="fab fa-facebook"></i>
+    </div>
+    <div class="links">
+        <p>Don't have an account yet?</p>
+        <button id="SignUp">Sign Up</button> 
+    </div>
+</div>
     <script src="script.js"></script>
-
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const signUpButton=document.getElementById('signUpButton');
-const signIpButton=document.getElementById('signInButton');
-const signInForm=document.getElementById('signIn');
-const signUpForm=document.getElementById('signUp');
-signUpButton.addEventListener('click',function(){
-    signInForm.style.display="none";
-    signUpForm.style.display="block";
 
-})
-signInForm.addEventListener('click',function(){
-    signInForm.style.display="block";
-    signUpForm.style.display="none";
-
-})
+const SignUpButton = document.getElementById('SignUp'); 
+const SignInButton = document.getElementById('SignIn'); 
+const signInForm = document.getElementById('signIn'); 
+const signUpForm = document.getElementById('signUp'); 
 
 
+SignUpButton.addEventListener('click', function () {
+    signInForm.style.display = "none"; 
+    signUpForm.style.display = "block"; 
+});
+
+
+SignInButton.addEventListener('click', function () {
+    signUpForm.style.display = "none"; 
+    signInForm.style.display = "block";
+});
